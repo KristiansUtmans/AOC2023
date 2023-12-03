c736ec05a8671dbac1d1ad0880d799bf107f0263
Kristians Utmans <kristians.utmans@gmail.com>
2023-12-03 21:59:01 +0200

    Removing comments

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,20 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         // Day 1
-//        int dayOnePartOneSolution = dayOne.Solution.solvePartOne();
-//        int dayOnePartTwoSolution = dayOne.Solution.solvePartTwo();
-//        System.out.println("Day 1 part 1 solution: " + dayOnePartOneSolution);
-//        System.out.println("Day 1 part 2 solution: " + dayOnePartTwoSolution);
-//
-//        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-//
-//        // Day 2
-//        int dayTwoPartOneSolution = dayTwo.Solution.solvePartOne();
-//        int dayTwoPartTwoSolution = dayTwo.Solution.solvePartTwo();
-//        System.out.println("Day 2 part 1 solution: " + dayTwoPartOneSolution);
-//        System.out.println("Day 2 part 2 solution: " + dayTwoPartTwoSolution);
-//
-//        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        int dayOnePartOneSolution = dayOne.Solution.solvePartOne();
+        int dayOnePartTwoSolution = dayOne.Solution.solvePartTwo();
+        System.out.println("Day 1 part 1 solution: " + dayOnePartOneSolution);
+        System.out.println("Day 1 part 2 solution: " + dayOnePartTwoSolution);
+
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+        // Day 2
+        int dayTwoPartOneSolution = dayTwo.Solution.solvePartOne();
+        int dayTwoPartTwoSolution = dayTwo.Solution.solvePartTwo();
+        System.out.println("Day 2 part 1 solution: " + dayTwoPartOneSolution);
+        System.out.println("Day 2 part 2 solution: " + dayTwoPartTwoSolution);
+
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
 
         // Day 3
         int dayThreePartOneSolution = dayThree.Solution.solvePartOne();
